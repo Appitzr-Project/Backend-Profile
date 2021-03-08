1151dce8a427eea130f1944725613af85d82f547
JuniYadi <juniyadi94@gmail.com>
2021-03-08 15:17:53 +0700

    Move API Health Check to Route Index

--- route/index.ts
+++ route/index.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import { Request, Response } from 'express';
 import { profileIndex } from '../controller/profileController';
 
 // Route Declare
@@ -6,6 +7,15 @@ const route = express.Router();
 
 // Route List
 route.get('/', profileIndex);
+
+// health check api
+route.get('/health-check', (req: Request, res: Response) => {
+    return res.status(200).json({
+        code: 200,
+        message: 'success',
+        headers: req.headers
+    });
+})
 
 // export all route
 export default route;
