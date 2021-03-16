9a6ffc0853e789f3da8aa31d6d8c903cad587d45
JuniYadi <juniyadi94@gmail.com>
2021-03-16 18:05:57 +0700

    Update ProfileController

--- controller/profileController.ts
+++ controller/profileController.ts
@@ -1,26 +1,43 @@
 import { Response, NextFunction } from "express";
 import { body, validationResult, ValidationChain } from 'express-validator';
-import { venueProfile, venueProfileModel } from "@appitzr-project/db-model";
+import { cultureCategory, venueAttribute, venueProfile, venueProfileModel } from "@appitzr-project/db-model";
 import { RequestAuthenticated, validateGroup } from "@base-pojokan/auth-aws-cognito";
 import * as AWS from 'aws-sdk';
 import { v4 as uuidv4 } from 'uuid';
 
 // declare database dynamodb
-const ddb = new AWS.DynamoDB.DocumentClient({ endpoint: process.env.DYNAMODB_LOCAL });
+const ddb = new AWS.DynamoDB.DocumentClient({ endpoint: process.env.DYNAMODB_LOCAL, convertEmptyValues: true });
 
 /**
- * Venue Profile Store Validation For Express Validator
+ * Venue Profile Store Validation with Express Validator
  */
-export const profileStoreValidate : ValidationChain[] =  [
-  body('venueName').isString().notEmpty(),
-  body('bankBSB').isString().notEmpty(),
-  body('bankName').isString().notEmpty(),
-  body('bankAccountNo').isString().notEmpty(),
-  body('phoneNumber').isString().notEmpty(),
-  body('address').isString().notEmpty(),
-  body('postalCode').isNumeric().notEmpty(),
-  body('long').isNumeric().notEmpty(),
-  body('lat').isNumeric().notEmpty(),
+export const profileStoreValidate : ValidationChain[] = [
+  body('venueName').notEmpty().isString(),
+  body('bankBSB').notEmpty().isString(),
+  body('bankName').notEmpty().isString(),
+  body('bankAccountNo').notEmpty().isString(),
+  body('phoneNumber').notEmpty().isString(),
+  body('address').notEmpty().isString(),
+  body('postalCode').notEmpty().isNumeric(),
+  body('long').notEmpty().isNumeric(),
+  body('lat').notEmpty().isNumeric(),
+  body('cultureCategory').notEmpty().isIn(cultureCategory),
+];
+
+/**
+ * Venue Profile Update Validation with Express Validator
+ */
+export const profileUpdateValidate : ValidationChain[] = [
+  body('venueName').notEmpty().isString(),
+  body('bankBSB').notEmpty().isString(),
+  body('bankName').notEmpty().isString(),
+  body('bankAccountNo').notEmpty().isString(),
+  body('phoneNumber').notEmpty().isString(),
+  body('address').notEmpty().isString(),
+  body('postalCode').notEmpty().isNumeric(),
+  body('long').notEmpty().isNumeric(),
+  body('lat').notEmpty().isNumeric(),
+  body('cultureCategory').notEmpty().isIn(cultureCategory),
 ];
 
 /**
@@ -88,25 +105,29 @@ export const profileStore = async (
     // get input
     const venue : venueProfile = req.body;
 
+    // venue profile input with typescript definition
+    const venueInput : venueProfile = {
+      id: uuidv4(),
+      cognitoId: userDetail?.sub,
+      venueEmail: userDetail?.email,
+      venueName: venue.venueName,
+      bankBSB: venue.bankBSB,
+      bankName: venue.bankName,
+      bankAccountNo: venue.bankAccountNo,
+      phoneNumber: venue.phoneNumber,
+      address: venue.address,
+      postalCode: venue.postalCode,
+      long: venue.long,
+      lat: venue.lat,
+      cultureCategory: venue.cultureCategory,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString()
+    }
+
     // dynamodb parameter
     const paramsDB : AWS.DynamoDB.DocumentClient.PutItemInput = {
       TableName: venueProfileModel.TableName,
-      Item: {
-        id: uuidv4(),
-        cognitoId: userDetail?.sub,
-        venueEmail: userDetail?.email,
-        venueName: venue.venueName,
-        bankBSB: venue.bankBSB,
-        bankName: venue.bankName,
-        bankAccountNo: venue.bankAccountNo,
-        phoneNumber: venue.phoneNumber,
-        address: venue.address,
-        postalCode: venue.postalCode,
-        long: venue.long,
-        lat: venue.lat,
-        createdAt: new Date().toISOString(),
-        updatedAt: new Date().toISOString()
-      },
+      Item: venueInput,
       ConditionExpression: 'attribute_not_exists(venueEmail)'
     }
 
@@ -130,6 +151,78 @@ export const profileStore = async (
     }
 
     // return default error
+    next(e);
+  }
+};
+
+
+export const profileUpdate = async (
+  req: RequestAuthenticated,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // validate group
+    const userDetail = await validateGroup(req, "venue");
+
+    // exapress validate input
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // get input
+    const venue : venueProfile = req.body;
+
+    // dynamodb parameter
+    const paramsDB : AWS.DynamoDB.DocumentClient.UpdateItemInput = {
+      TableName: venueProfileModel.TableName,
+      Key: {
+        venueEmail: userDetail.email,
+        cognitoId: userDetail.sub
+      },
+      UpdateExpression: `
+        set
+          venueName = :vn,
+          bankBSB = :bbsb,
+          bankName = :bn,
+          bankAccountNo = :ban,
+          phoneNumber = :pn,
+          address = :adrs,
+          postalCode = :pc,
+          maplong = :mlong,
+          maplat = :mlat,
+          cultureCategory = :ccat,
+          updatedAt = :ua
+      `,
+      ExpressionAttributeValues: {
+        ':vn': venue.venueName,
+        ':bbsb': venue.bankBSB,
+        ':bn': venue.bankName,
+        ':ban': venue.bankAccountNo,
+        ':pn': venue.phoneNumber,
+        ':adrs': venue.address,
+        ':pc': venue.postalCode,
+        ':mlong': venue.long,
+        ':mlat': venue.lat,
+        ':ccat': venue.cultureCategory,
+        ':ua': new Date().toISOString(),
+      },
+      ReturnValues: 'UPDATED_NEW',
+      ConditionExpression: 'attribute_exists(venueEmail)'
+    }
+
+    // save data to database
+    const queryDB = await ddb.update(paramsDB).promise();
+
+    // return result
+    return res.status(200).json({
+      code: 200,
+      message: 'success',
+      data: queryDB?.Attributes
+    });
+  } catch (e) {
+    console.log(e);
     next(e);
   }
 };
