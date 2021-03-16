c8d98ee0a45336e549718a0fd08f1c7a1545339d
JuniYadi <juniyadi94@gmail.com>
2021-03-16 18:35:39 +0700

    Update Serverless.ts - Using Yarn

--- serverless.ts
+++ serverless.ts
@@ -58,6 +58,7 @@ const serverlessConfiguration: Serverless = {
     },
     custom: {
         webpack: {
+            packager: 'yarn',
             webpackConfig: './webpack.config.js',
             includeModules: true,
         },
