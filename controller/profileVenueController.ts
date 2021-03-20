277271d2061b80e9baa26ec0355638b2a62f76e5
JuniYadi <juniyadi94@gmail.com>
2021-03-20 18:46:13 +0700

    Update profileVenueController - Change Profile Picture

--- controller/profileVenueController.ts
+++ controller/profileVenueController.ts
@@ -4,9 +4,16 @@ import { cultureCategory, venueAttribute, venueProfile, venueProfileModel } from
 import { RequestAuthenticated, userDetail, validateGroup } from "@base-pojokan/auth-aws-cognito";
 import * as AWS from 'aws-sdk';
 import { v4 as uuidv4 } from 'uuid';
+import * as Multer from 'multer';
+import * as fs from 'fs';
 
 // declare database dynamodb
 const ddb = new AWS.DynamoDB.DocumentClient({ endpoint: process.env.DYNAMODB_LOCAL, convertEmptyValues: true });
+
+/**
+ * Multer Storage and Upload
+ */
+const multerUpload = Multer({ dest: '/tmp' });
 
 /**
  * Venue Profile Store Validation with Express Validator
@@ -38,6 +45,38 @@ export const profileVenueUpdateValidate : ValidationChain[] = [
   body('mapLong').notEmpty().isNumeric(),
   body('mapLat').notEmpty().isNumeric(),
   body('cultureCategory').notEmpty().isIn(cultureCategory),
+];
+
+/**
+ * Validator Image Input For Single File
+ * Maximum File Size Limit 5 Mb
+ * And Format .jpeg, .jpg, and .png
+ */
+export const profilePictureVenueValidate : any[] = [
+  // single upload middleware
+  multerUpload.single('profilePicture'),
+
+  // express validator size and mime type
+  body('profilePicture')
+    .custom((value, {req}) => {
+      // size limit 5 MB in byte
+      const fileLimit : number = 5242880;
+
+      // check if file exist
+      // and size file under fileLimit
+      if(req.file && req.file.size < fileLimit) {
+        // check mime type file
+        if(req.file.mimetype === 'image/png'){
+          return '.png';
+        } else if(req.file.mimetype === 'image/jpeg'){
+          return '.jpeg';
+        } else {
+          throw new Error('Format Allowed: .jpeg, .jpg or .png');
+        }
+      } else {
+        throw new Error('Upload File Required With Maximum File Size 5 MB/File.!');
+      }
+    }),
 ];
 
 /**
@@ -207,6 +246,97 @@ export const profileVenueUpdate = async (
         ':mlong': venue.mapLong,
         ':mlat': venue.mapLat,
         ':ccat': venue.cultureCategory,
+        ':ua': new Date().toISOString(),
+      },
+      ReturnValues: 'UPDATED_NEW',
+      ConditionExpression: 'attribute_exists(venueEmail)'
+    }
+
+    // save data to database
+    const queryDB = await ddb.update(paramsDB).promise();
+
+    // return result
+    return res.status(200).json({
+      code: 200,
+      message: 'success',
+      data: queryDB?.Attributes
+    });
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+};
+
+
+export const profileVenueChange = async (
+  req: RequestAuthenticated,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // validate group
+    const user = userDetail(req);
+
+    // exapress validate input
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // get file upload
+    // types from Global Express
+    const fileUpload : Express.Multer.File = req.file;
+
+    // file extention
+    let fileExtention : string;
+    if(fileUpload.mimetype === 'image/png' ) {
+      fileExtention = '.png';
+    } else if(fileUpload.mimetype === 'image/jpeg') {
+      fileExtention = '.jpeg';
+    }
+
+    // generate filename with uuid
+    const fileName : string = uuidv4() + fileExtention;
+
+    // generate path directory
+    const date  = new Date();
+    const year  : number  = date.getFullYear();
+    const month : number  = date.getMonth() + 1;
+    const day   : number  = date.getDate();
+
+    // generate filename with path
+    const fullFileName : string = `${year}/${month}/${day}/${fileName}`;
+    // generate URL with Full Name
+    const fileURL = 'https://' + process.env.AWS_S3_BUCKET + '/' + fullFileName;
+
+    /**
+     * Upload File to AWS S3
+     */
+    const S3 = new AWS.S3();
+    
+    // S3 Action Upload Fsile
+    await S3.upload({
+      ACL: 'public-read',
+      Bucket: process.env.AWS_S3_BUCKET,
+      Body: Buffer.from(fs.readFileSync(fileUpload.path, 'base64'), 'base64'),
+      Key: fullFileName,
+      ContentType: fileUpload.mimetype,
+    }).promise();
+
+    // dynamodb parameter
+    const paramsDB : AWS.DynamoDB.DocumentClient.UpdateItemInput = {
+      TableName: venueProfileModel.TableName,
+      Key: {
+        venueEmail: user.email,
+        cognitoId: user.sub
+      },
+      UpdateExpression: `
+        set
+          profilePicture = :pp,
+          updatedAt = :ua
+      `,
+      ExpressionAttributeValues: {
+        ':pp': fileURL,
         ':ua': new Date().toISOString(),
       },
       ReturnValues: 'UPDATED_NEW',
