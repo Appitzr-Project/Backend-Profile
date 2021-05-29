ae397627455f6a20e0ea208393520115887f3aa2
Juni Yadi <juniyadi94@gmail.com>
2021-05-29 20:47:57 +0700

    Aws cognito mapping (#16)
    * Update Serverless - Ignore AWS-SDK
    
    * Update Handler - Print Error in Cloudwatch
    
    * Update Package

--- serverless.ts
+++ serverless.ts
@@ -122,12 +122,18 @@ const serverlessConfiguration: Serverless = {
     },
     custom: {
         webpack: {
+            packager: 'npm',
             webpackConfig: './webpack.config.js',
             includeModules: {
                 forceExclude: [
                     'aws-sdk'
                 ],
             },
+            packagerOptions: {
+                scripts: [
+                    'rm -rf node_modules/aws-sdk'
+                ]
+            }
         },
         project: {
             cognito: 'arn:aws:cognito-idp:${opt:region, "ap-southeast-2"}:${env:AWS_ACCOUNT_ID}:userpool/${env:COGNITO_POOL_ID}',
